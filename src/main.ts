44bb5f477373671fa8f5fc89e4e9e4c08f8f9b7b
TheCodeTherapy <me@mgz.me>
2022-08-15 01:33:25 +0100

    first commit

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { getOctokit } from "@actions/github";
+
+import { collectDeploymentContext } from "./lib/context";
+import { getRequiredInput } from "./lib/input";
+
+import { run, Step } from "./steps";
+
+const context = collectDeploymentContext();
+console.log(`targeting ${context.owner}/${context.repo}`);
+
+const token = getRequiredInput("token");
+const github = getOctokit(token, {
+  previews: ["ant-man-preview", "flash-preview"],
+});
+
+const step = getRequiredInput("step") as Step;
+run(step, github, context);
